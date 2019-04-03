986a7f25abe538812e3df3a24e43c3258701cb84
Adrian de Vera Alonzo <aalonzo3@gatech.edu>
2019-04-02 19:12:40 -0700

    Initial Commit

---
+++ IMGURsnipper/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  IMGURsnipper
+//
+//  Created by Adrian de Vera Alonzo on 4/2/19.
+//  Copyright © 2019 Adrian Alonzo. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
